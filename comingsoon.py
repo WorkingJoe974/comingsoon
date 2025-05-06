918145a9a98c064cb790f8c1a78a6d199e66ced4
Nathaniel Stambaugh <macdaddy101@icloud.com>
2025-05-05 21:59:40 -0400

    added remote debugging port in the chrome options argument

--- comingsoon.py
+++ comingsoon.py
@@ -119,6 +119,7 @@ async def check_stock():
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--remote-debugging-port=9222")
 
         channel = bot.get_channel(CHANNEL_ID)
 
